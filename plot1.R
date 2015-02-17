0d2c7fc3b27a929da4524f962b9c5b06f3231e9f
jewlf <jewlf@sandyflat.net>
2015-02-17 09:31:19 -0500

    Cleaned up formatting

--- plot1.R
+++ plot1.R
@@ -1,23 +1,25 @@
-## Project 2. plot 1 - Total emissions from 1999 to 2008
-# Read data file
+## Project 2. plot 1 - Total emissions from 1999 to 2008 for US
+# Read data file (only this one needed for this plot)
 NEI <- readRDS("summarySCC_PM25.rds")
 
-# Get sums by year
+# Get sums by year into a new smaller table
 year_totals <- aggregate(NEI$Emissions, by=list(NEI$year), FUN=sum)
 
 # Apply meaningful column names
 colnames(year_totals) <- c("year", "tons")
 
 # Open PNG plot device with desired name and image dimensions
-png("plot2.png", width = 480, height = 480)
+png("plot1.png", width = 480, height = 480)
 
 # Create plot
-plot(year_totals$year, (year_totals$tons / 1000000), type="p", pch=19, xlab="Year", ylab="Total (million tons)", main="Total Emissions by Year", xaxp=c(1999, 2008, 3))
+plot(year_totals$year, (year_totals$tons / 1000000), type="p", pch=19, xlab="Year",
+  ylab="Total (million tons)", main="US Total Emissions by Year",
+  xaxp=c(1999, 2008, 3))
 
 # Calculate linear model
 model <- lm((year_totals$tons / 1000000) ~ year, year_totals )
 
-# Apply model line to plot
+# Apply model line to the plot
 abline(model, lwd = 2, col="blue")
 
 # Close the device
